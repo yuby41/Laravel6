2a93b082563d6eddd503aec2156c736df0438989
Yualbe Luis <lois21us@gmail.com>
2021-10-07 11:17:59 +0200

    Logica de contrladores y Vistas con datos de prueba

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,23 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
     public function index(){
 
-        
+        $users = User::latest()->get();
 
+        return view('users.index', ['users' => $users]);
 
     }
 
-    public function store(){
+    public function store(Request $request){
 
+        User::create[[
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ]];
+
+        return back();
         
     }
 
-    public function destroy(){
+    public function destroy(User $user){
 
+        $user->delete();
+
+        return back();
         
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel6</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+       
+    </head>
+    <body>
+        <div id="container">
+            <div class="row">
+                <div class="col-sm-8 mx-auto">
+
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>Eliminar</td>
+                        </tr>
+                        @endforeach
+                </table>
+                </div>
+
+
+
+        
+    </body>
+</html>
