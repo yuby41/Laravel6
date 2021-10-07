cda5e051e8410831e4a3a65a97283f5e704efaa3
Yualbe Luis <lois21us@gmail.com>
2021-10-07 05:06:14 +0200

    Creando las rutas y los contrladores

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+
+        
+
+
+    }
+
+    public function store(){
+
+        
+    }
+
+    public function destroy(){
+
+        
+    }
+}
